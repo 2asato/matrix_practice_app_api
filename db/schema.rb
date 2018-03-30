f02e190a28e9b8030a74aef08ca1bed431e2a930
aaronsato <2asato@gmail.com>
2018-03-29 23:40:40 -0700

    seeded db added postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180326221849) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "characters", force: :cascade do |t|
     t.string "name"
     t.string "description"
